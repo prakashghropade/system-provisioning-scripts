ef7522f9d6fa948faa40a3c8728055bda4a964ee
prakashghorpade <prakashghorpade2001@gmail.com>
2026-05-25 11:32:58 +0530

    updated the jenkines installation file

--- installed_jenkines.sh
+++ installed_jenkines.sh
@@ -35,14 +35,14 @@ echo "============================================"
 echo " Downloading Jenkins GPG key..."
 echo "============================================"
 sudo wget -O /etc/apt/keyrings/jenkins-keyring.asc \
-https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key
+  https://pkg.jenkins.io/debian-stable/jenkins.io-2026.key
 
 echo "============================================"
 echo " Adding Jenkins repository..."
 echo "============================================"
-echo "deb [signed-by=/etc/apt/keyrings/jenkins-keyring.asc] \
-https://pkg.jenkins.io/debian-stable binary/" | \
-sudo tee /etc/apt/sources.list.d/jenkins.list > /dev/null
+echo "deb [signed-by=/etc/apt/keyrings/jenkins-keyring.asc]" \
+  https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+  /etc/apt/sources.list.d/jenkins.list > /dev/null
 
 echo "============================================"
 echo " Updating package list again..."
